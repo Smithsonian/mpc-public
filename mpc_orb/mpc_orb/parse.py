1101fa6e4ac5f2690212fa3888f29352dca4b4a0
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-04 11:04:54 -0500

    upgrading parse to allow descriptions

--- mpc_orb/mpc_orb/parse.py
+++ mpc_orb/mpc_orb/parse.py
@@ -17,12 +17,15 @@ import numpy as np
 # -----------------------
 from . import interpret
 from . import validate_mpcorb
+from . import filepaths
 
 # Class to parse JSONs
 # -----------------------
 class MPCORB():
 
     def __init__(self, arg=None ):
+        self.schema_json = None
+        
         """ If an argument is supplied at initiation, go ahead and parse ( & validate ) """
         if arg is not None:
             self.parse( arg)
@@ -49,7 +52,57 @@ class MPCORB():
         
         # provide useful quantities as attributes
         self._add_various_attributes(json_dict)
-    
+        
+    def describe(self, variable_name):
+        """
+        Parse an individual variable/attribute of the MPCORB object
+        
+        Reads the "description" of a variable from the schema
+        
+        inputs:
+        -------
+        variable_name: string
+        
+        action:
+        --------
+        returns dictionary containing any available "descriptive" elements
+        
+        """
+        if self.schema_json is None:
+            self.schema_json = validate_mpcorb.load_json( filepaths.mpcorb_schema )
+                    
+                    
+        # Find the appropriate variable name to use to search for a description ...
+        # (i) If variable_name is a top-level property such as 'designation_data', 'COM', etc
+        if variable_name in self.schema_json['properties'].keys():
+            return  {variable_name: {'description':self.schema_json['properties'][variable_name]['description'] }}
+
+        # (ii) If variable_name is one of the defined-schema variables
+        if variable_name in self.schema_json['$defs']:
+            def_var_name = variable_name
+            
+        # (iii) If variable_name is an attribute of COM
+        elif variable_name in self.COM.__dict__:
+            def_var_name = self.schema_json['properties']['COM']['properties']['coefficient_specification']['properties'][variable_name]['$ref'].replace('#/$defs/','')
+
+        # (iv) If variable_name is an attribute of CAR
+        elif variable_name in self.CAR.__dict__:
+            def_var_name = self.schema_json['properties']['CAR']['properties']['coefficient_specification']['properties'][variable_name]['$ref'].replace('#/$defs/','')
+
+        # (v) Hopefully we never see this ...
+        else:
+            def_var_name = None
+            
+            
+        # Extract a description
+        return  {variable_name: None } if def_var_name is None else \
+                {variable_name: self._clean(self.schema_json['$defs'][def_var_name])}
+            
+
+    def _clean(self,d):
+        """ """
+        return { k: self._clean(v) if isinstance(v, dict) else v for k,v in d.items() if k not in ['type']}
+        
 
     
     def _add_various_attributes(self,json_dict):
@@ -57,57 +110,82 @@ class MPCORB():
         expose various quantities from mpcorb.json as object attributes
         """
         
-        # make top-level quantities available as object attributes
+        # make top-level quantities available as object attributes (excluding CAR & COM)
         for k,v in json_dict.items():
-            self.__dict__[k] = v
+            if k not in ["COM", "CAR"]:
+                self.__dict__[k] = v
 
-        # These coord-types are both required in a valid input mpc_orb.json
-        for coord_attr in ["COM", "CAR"]:
-            self._populate_coord_components(coord_attr)
+        # Create CAR & COM objects within MPCORB object:
+        # - this ultimately allows you to evaluate M.COM.coefficient_values, etc
+        for k in ["COM", "CAR"]:
+            self.__dict__[k] = COORD(json_dict[k])
+            
+        # For convenience, expose individual "elements" (E.g. "x" or "e") from COORD in MPCORB
+        # E.g. MPCORB.x == MPCORB.CAR.x
+        # *** NOT exposing "bulk" quantities (e.g. "coefficient_values" array),
+        # *** as they will conflict between COM & CAR
+        for k in ["COM", "CAR"]:
+            for name in self.__dict__[k].coefficient_names:
+                self.__dict__[name] = self.__dict__[k].__dict__[name]
                     
         # Is there some other stuff we want to provide as convenient attributes?
         # - E.g. stats on N_Oppositions, N_Observations, etc ?
         
+        return True
+
+
+
+class COORD():
+    """ Class to hold COM or CAR elements """
+
+    def __init__(self, coord_dict ):
+        """ """
+        self._populate_coord_components(coord_dict)
         
-    def _populate_coord_components(self,coord_attr):
-        """ populate various components for specific representation """
-        
+    def _populate_coord_components(self,coord_dict):
+        """ populate various components for COORD representation """
+
+        # make top-level quantities available as object attributes
+        # - E.g. 'coefficient_names', 'coefficient_values', 'coefficient_uncertainties', 'eigenvalues', 'covariance'
+        for k,v in coord_dict.items():
+            self.__dict__[k] = v
+
         # populate square CoV matricees
-        self.__dict__[coord_attr]['covariance_array'] = self._generate_square_CoV( coord_attr )
+        self._generate_square_CoV(  )
 
-        # populate element array
-        self.__dict__[coord_attr]['element_array'] = self._generate_element_array( coord_attr )
+        # populate element dictionary
+        self._generate_element_dict(  )
 
-        # provide uncertainty (CoV diag)
-        # *** MJP:2023-01-24: Possibly unnecessary: I think "Uncertainty" already exists...
-        self.__dict__[coord_attr]['uncertainty'] = self._generate_uncertainty( coord_attr )
-    
-    def _generate_square_CoV(self, coord_attr ):
+        # populate individual coordinate dictionaries (e.g. COORD.x)
+        self._generate_individual_element_dicts()
+
+    def _generate_square_CoV(self, ):
         """ populate square array from triangular elements """
-        num_params       = self.__dict__['orbit_fit_statistics']['numparams']
-        covariance_array = np.empty( [num_params,num_params] )
+        num_params                        = len(self.__dict__['coefficient_values'])
+        self.__dict__['covariance_array'] = np.empty( [num_params,num_params] )
         for i in range(num_params):
             for j in range(i,num_params):
-                covariance_array[i,j] = covariance_array[j,i] = self.__dict__[coord_attr]['covariance']['cov%d%d' % (i,j)]
-                
-        return covariance_array
-
-    def _generate_element_array(self,  coord_attr ):
-        """ turn element dict into numpy array (with fixed ordering) """
+                self.__dict__['covariance_array'][i,j] = \
+                self.__dict__['covariance_array'][j,i] = \
+                    self.__dict__['covariance']['cov%d%d' % (i,j)]
         
-        if 'element_order' in self.__dict__[coord_attr]:
-            element_order = self.__dict__[coord_attr]['element_order']
-        else:
-            element_order = ['x','y','z','vx','vy','vz'] if coord_attr == 'CAR' else ['q','e','i','node','argperi','peri_time']
-        return np.array( [ self.__dict__[coord_attr]['elements'][key] for key in element_order] )
-        
-    def _generate_uncertainty(self, coord_attr ):
-        """ extract sqrt of diag elements in CoV-arry as uncertainty array
-            *** MJP:2023-01-24: Possibly unnecessary: I think "Uncertainty" already exists...
+    def _generate_element_dict(self, ):
+        """ turn element lists into dictionary
+            N.B. Input JSON contains lists/arrays, so here we provide a dictionary as well
         """
-        return np.sqrt( self.__dict__[coord_attr]['covariance_array'].diagonal() )
-
-
+        self.__dict__['element_dict'] =  \
+            { k:{"val":v, "unc":u} for k,v,u in \
+                    zip(    self.__dict__["coefficient_names"],\
+                            self.__dict__["coefficient_values"],\
+                            self.__dict__["coefficient_uncertainties"]) }
+        
+    def _generate_individual_element_dicts(self, ):
+        """ turn element dictionary entries into object-attributes
+            E.g. COORD.x == COORD
+        """
+        for k in self.__dict__['element_dict']:
+            self.__dict__[k]=self.__dict__['element_dict'][k]
+       
 
 
 
--- mpc_orb/tests/test_parse.py
+++ mpc_orb/tests/test_parse.py
@@ -8,7 +8,7 @@ from os.path import join, dirname, abspath
 
 # Local imports
 # -----------------------
-from mpc_orb import MPCORB
+from mpc_orb import MPCORB, COORD
 from mpc_orb import filepaths
 
 
@@ -30,50 +30,72 @@ def test_MPCORB_B(  ):
     Instantiate with file
     '''
   
-    # Loop over the defining mpcorb files
+    # Loop over the mpcorb files that are expect to "pass"
     # Attempt to instantiate using each ...
     for f in filepaths.test_pass_mpcorb:
         M = MPCORB(f)
         
         assert isinstance(M,MPCORB)
 
-"""
+
 def test_parse_C(  ):
     '''
     Test the parsing of mpcorb-jsons ...
     Check basic attributes
     '''
   
-    # Loop over the defining mpcorb files
+    # Loop over the mpcorb files that are expect to "pass"
     # Attempt to instantiate using each ...
-    for f in filepath_dict['mpcorb_defining_sample'][:1]:
+    for f in filepaths.test_pass_mpcorb:
         M = MPCORB(f)
-        for k in ['COM','CAR','nongrav_data', 'system_data', 'designation_data', 'magnitude_data', 'epoch_data']:
+        for k in ['COM','CAR','categorization', 'epoch_data', 'designation_data', 'magnitude_data', 'non_grav_booleans', 'orbit_fit_statistics', 'software_data', 'system_data']:
             assert hasattr(M,k)
             
-        # Check that coord type has expected keys in coord dict ...
-        for k in ['COM','CAR']:
-            for key in ['eigval', 'covariance', 'elements', 'element_order', 'numparams', 'rms']:
-                assert key in M.__dict__[k],f"M.__dict__[k]={M.__dict__[k]}"
-
 def test_parse_D(  ):
     '''
-    Test the parsing of mpcorb-jsons ...
-    Check added attributes
+    Test the CAR & COM sub-classes ...
+    Check element attributes
     '''
-  
-    # Loop over the defining mpcorb files
-    # Attempt to instantiate using each ...
-    for f in filepath_dict['mpcorb_defining_sample'][:1]:
+    for f in filepaths.test_pass_mpcorb:
         M = MPCORB(f)
+
+        for I,expected_names  in zip(   [M.COM, M.CAR], \
+                                        [["q","e","i","node","argperi","peri_time"], ["x","y","z","vx","vy","vz"] ]):
+
+            # Check that we have a "COORD" object
+            assert isinstance(I, COORD)
+
+            # Check that "I" has the expected "bulk" attributes ...
+            for key in ['coefficient_names', 'coefficient_values', 'coefficient_uncertainties', 'eigenvalues', 'covariance', 'covariance_array', 'element_dict']:
+                assert hasattr(I, key)
+
+            # Check that "I" has the expected individual attributes ...
+            for name in expected_names:
+                assert hasattr(I, name)
+                assert isinstance( I.__dict__[name], dict) and "val" in I.__dict__[name] and "unc" in I.__dict__[name]
+
+            # Check that the individual attribute values are the same as the element_dict entries
+            # E.g. I.element_dict["x"] == I.x
+            for name in expected_names:
+                assert I.element_dict[name] == I.__dict__[name]
+
+
+        # Double-check that the individual attributes (e.g. "x" or "e") are accessible directly from the MPCORB object, M
+        for name in ["q","e","i","node","argperi","peri_time"] + ["x","y","z","vx","vy","vz"] :
+            assert hasattr(M, name)
         
-        for k in ['COM','CAR']:
-            
-            # Check that coord type exists
-            assert hasattr(M,k), f"M.__dict__.items() = {M.__dict__.items()}"
 
-            # Check that coord type has added keys in coord dict ...
-            for key in ['covariance_array', 'element_array', 'uncertainty']:
-                assert key in M.__dict__[k],f"M.__dict__[k]={M.__dict__[k]}"
-"""
+def test_describe_A(  ):
+    '''
+    Test the describe function
+    '''
 
+    for f in filepaths.test_pass_mpcorb[:1]:
+        M = MPCORB(f)
+
+        for key in [ _ for _ in M.__dict__.keys() if _ != 'schema_json']:
+            description_dict = M.describe(key)
+            assert isinstance(description_dict, dict)
+            assert key in description_dict
+            assert isinstance(description_dict[key], dict), f'-----------------------------------{key}'
+            assert description_dict[key] is not None
