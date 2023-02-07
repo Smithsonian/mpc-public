8db5e424db9db8f030dcc7d30a7949eda0a93acf
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-07 18:17:35 -0500

    updated & simplified filepath structure: schema only

--- mpc_orb/mpc_orb/filepaths.py
+++ mpc_orb/mpc_orb/filepaths.py
@@ -2,31 +2,22 @@
 Defining filepaths used by mpc_orb
 """
 
-# Import third-party packages
+# Import standard packages
 # -----------------------
 import glob
-from os.path import join, dirname, abspath
 import os
 
-import mpc_orb.json_files.schema_json
-import mpc_orb.json_files.test_jsons.fail_mpcorb
-import mpc_orb.json_files.test_jsons.pass_mpcorb
+# Import(s) from this package
+# -----------------------
+import mpc_orb.schema_json
 
 # Directories
 # -----------------------
-schema_dir = mpc_orb.json_files.schema_json.__path__[0]
-fail_dir   = mpc_orb.json_files.test_jsons.fail_mpcorb.__path__[0]
-pass_dir   = mpc_orb.json_files.test_jsons.pass_mpcorb.__path__[0]
-
+schema_dir = mpc_orb.schema_json.__path__[0]
 
 # Filepaths / Lists-of-Filepaths
 # -----------------------
 # Filepath to json schema against which everything is validated
-mpcorb_schema = os.path.join(schema_dir, 'mpcorb_schema.json')
-
-# Filepaths to sample json files used in tests
-test_fail_mpcorb = glob.glob( fail_dir + "/*.json" )
-test_pass_mpcorb = glob.glob( pass_dir + "/*.json" )
-
-
+# - Gets the highest-numbered version in the directory
+mpcorb_schema = sorted( glob.glob( schema_dir + '/*.json' ) )[-1]
 
