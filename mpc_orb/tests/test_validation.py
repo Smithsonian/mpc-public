a36b631f3bf82d069a7806dad00c55e7af7ba74c
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-09 11:43:27 -0500

    incremental addns to test-validation

--- mpc_orb/tests/test_validation.py
+++ mpc_orb/tests/test_validation.py
@@ -9,6 +9,8 @@ Test validation schema
 import pytest
 import os
 import sys
+import pkgutil
+import json
 
 # Local imports
 # -----------------------
@@ -17,8 +19,13 @@ from mpc_orb import filepaths
 from . import filepaths_for_testing
 
 
-
-
+def load_package_json(filepath_relative_to_package):
+    ''' use pkgutil to get resource & return as dict '''
+    print("***"*33)
+    print(f'***load_package_json***: __package__={__package__} , filepath_relative_to_package = {filepath_relative_to_package}')
+    raw_bytes = pkgutil.get_data(__package__ , filepath_relative_to_package)
+    return json.loads(raw_bytes.decode('utf-8'))
+    
 
 # Lower level tests
 # -----------------------
@@ -38,8 +45,9 @@ def test_pass_file():
 
 def test_validation_A():
     ''' Test that a single, valid json-file successfully validates'''
-    assert validate_mpcorb.validate_mpcorb( filepaths_for_testing.test_pass_yarkovski ), \
-        f'could not validate {filepaths_for_testing.test_pass_yarkovski}'
+    data_dict = load_package_json(filepaths_for_testing.test_pass_yarkovski)
+    assert validate_mpcorb.validate_mpcorb( data_dict ), \
+        f'could not validate data_dict from {filepaths_for_testing.test_pass_yarkovski}'
 
 """
 
