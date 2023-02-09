7f130fbf844ddf7d07b31b60787af8168a81a51c
Christopher Moriarty <cmoriarty@cfa.harvard.edu>
2023-02-09 17:12:22 -0500

    Attempt to fix pypi error about long_description missing.

--- mpc_orb/setup.py
+++ mpc_orb/setup.py
@@ -1,18 +1,17 @@
 import setuptools
 
 setuptools.setup(
-	name='mpc_orb',
-	version='0.0.4',
-	description='For the validation and parsing of mpc_orb.json formatted data',
-	author='MJP:MPC',
-	author_email='mpayne@cfa.harvard.edu',
-	url='https://github.com/matthewjohnpayne/mpc_orb',
-	install_requires=[
-		'jsonschema',
-		'numpy',
-		'pytest'],
-	packages=setuptools.find_packages(),
+    name='mpc_orb',
+    version='0.0.4',
+    long_description='For the validation and parsing of mpc_orb.json formatted data',
+    author='MJP:MPC',
+    author_email='mpayne@cfa.harvard.edu',
+    url='https://github.com/matthewjohnpayne/mpc_orb',
+    install_requires=[
+        'jsonschema',
+        'numpy',
+        'pytest'],
+    packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={'' : ['mpc_orb/schema_json/*.json']},
-	zip_safe=False)
-
+    package_data={'': ['mpc_orb/schema_json/*.json']},
+    zip_safe=False)
