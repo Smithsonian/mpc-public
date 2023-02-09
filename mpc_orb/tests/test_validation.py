a77b47a261209479a2c2bf9d22d4a5d5cd3be52e
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-09 12:34:31 -0500

    increasing the number of validation test

--- mpc_orb/tests/test_validation.py
+++ mpc_orb/tests/test_validation.py
@@ -21,8 +21,6 @@ from . import filepaths_for_testing
 
 def load_package_json(filepath_relative_to_package):
     ''' use pkgutil to get resource & return as dict '''
-    print("***"*33)
-    print(f'***load_package_json***: __package__={__package__} , filepath_relative_to_package = {filepath_relative_to_package}')
     raw_bytes = pkgutil.get_data(__package__ , filepath_relative_to_package)
     return json.loads(raw_bytes.decode('utf-8'))
     
@@ -34,22 +32,13 @@ def test_schema():
     assert validate_mpcorb.load_schema(), \
         f'could not open schema'
 
-"""
-def test_pass_file():
-    ''' Test that a single, valid json-file successfully loads'''
-    assert os.path.isfile(filepaths_for_testing.test_pass_mpcorb[0])
-    #assert validate_mpcorb.load_json( filepaths_for_testing.test_pass_mpcorb[0] ), \
-    #    f'could not open {filepaths_for_testing.test_pass_mpcorb[0]}'
-
-"""
-
 def test_validation_A():
     ''' Test that a single, valid json-file successfully validates'''
     data_dict = load_package_json(filepaths_for_testing.test_pass_yarkovski)
     assert validate_mpcorb.validate_mpcorb( data_dict ), \
         f'could not validate data_dict from {filepaths_for_testing.test_pass_yarkovski}'
 
-"""
+
 
 # High level tests
 # -----------------------
@@ -58,9 +47,14 @@ def test_validation_B(  ):
     Test that all valid JSON-files in the 'test_pass_mpcorb' directory pass validation as expected
     '''
   
-    # The filepath_dict from filepaths contains the locations of a number of test files
-    for f in filepaths_for_testing.test_pass_mpcorb:
-        validate_mpcorb.validate_mpcorb(f)
+    # Get all of the json filenames defined in filepaths_for_testing that have '...pass...' in the name
+    # NB : At the time of writing there was only one filepath ... in which case test_validation_B == test_validation_A
+    for k in filepaths_for_testing.__dict__:
+        if "__" not in k and "pass" in k:
+            
+            data_dict = load_package_json(filepaths_for_testing.__dict__[k])
+            assert validate_mpcorb.validate_mpcorb( data_dict )
+
 
 
 @pytest.mark.xfail
@@ -70,4 +64,4 @@ def test_validation_C(  ):
     '''
     for f in filepaths_for_testing.test_fail_mpcorb:
         validate_mpcorb.validate_mpcorb(f)
-"""
+
