f3940cb68176a1e6d26f971f0c9ce6012c68cc35
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-11 14:22:46 -0500

    variations w/ setup.py ... 0.1.2

--- mpc_orb/setup.py
+++ mpc_orb/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mpc_orb',
-    version='0.1.1',
+    version='0.1.2  ',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='MJP:MPC',
@@ -16,5 +16,5 @@ setuptools.setup(
         'numpy',
         'pytest'],
     packages=setuptools.find_packages(),#where="mpc_orb"),
-    package_data={'': ['mpc_orb/schema_json/*.json']},
+    package_data={"": ["*.json"]},
     zip_safe=False)
