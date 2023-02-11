8e5524f2a99b5a7c223712b7d6934f96e9566fc6
Matt Payne <matthewjohnpayne@gmail.com>
2023-02-11 13:30:55 -0500

    trying with only package data

--- mpc_orb/setup.py
+++ mpc_orb/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mpc_orb',
-    version='0.0.6',
+    version='0.0.7',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='MJP:MPC',
