005df927bfd483fe5e4837eec07c88b8f7c6c1ab
Matt Payne <matthewjohnpayne@gmail.com>
2026-02-02 19:17:31 -0500

    Deployed 6f6aa82 with MkDocs version: 1.6.1

---
+++ javascript/force-ipynb-download.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[href$=".ipynb"]').forEach((a) => {
+    const url = new URL(a.getAttribute("href"), window.location.href);
+    if (url.origin !== window.location.origin) return;
+    a.setAttribute("download", url.pathname.split("/").pop());
+  });
+});
